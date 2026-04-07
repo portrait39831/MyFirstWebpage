ee75cc4b9c7b664caabdcc03ba9ad3060a8a35f7
portrait39831 <jangheejun2006@naver.com>
2026-04-07 21:56:52 +0900

    edit list

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const openSet = document.getElementById("openSetting");
 const modal = document.getElementById("settingContainer");
 const closeSet = document.getElementById("closeSetting");
 const addCookie = document.getElementById("addCookie");
-const container = document.getElementById("imageContainer");
+const container = document.getElementById("list");
 
 openSet.addEventListener ('click', () => {
     modal.classList.add('active');
@@ -23,8 +23,20 @@ window.addEventListener('click', (e) => {
 
 
 addCookie.addEventListener('click', () => {
-    const newImg = document.createElement('img');
-    newImg.src = 'icon/icon-sns-youtube2.svg';
+    const newRow = document.createElement('div');
+    newRow.className = 'row';
 
-    container.appendChild(newImg);
+    newRow.innerHTML = `
+    <img src="icon/icon-sns-youtube2.svg">
+    <div class="row-text">
+      <strong>새로운 아이템</strong><br>
+      <small>방금 추가된 항목입니다.</small>
+    </div>
+    <button class="delete-btn">삭제</button>`;    
+    
+    container.appendChild(newRow);
+
+    newRow.querySelector('.delete-btn').addEventListener('click', () => {
+    newRow.remove();
+    });
 })
